14777167a09e6bb17c58cb8162ec4653c25488e9
Tristan Elliott <>
2023-07-24 12:06:08 -0300

    final clean up for blogpost

--- app/src/main/java/com/example/clicker/network/websockets/TwitchWebSocket.kt
+++ app/src/main/java/com/example/clicker/network/websockets/TwitchWebSocket.kt
@@ -39,9 +39,9 @@ class TwitchWebSocket(): WebSocketListener() {
     override fun onOpen(webSocket: WebSocket, response: Response) {
         super.onOpen(webSocket, response)
         webSocket.send("CAP REQ :twitch.tv/tags twitch.tv/commands");
-        //7rgcke18dgqlo0tiinetfwq6m0ge1c
+        //
         //todo: add the User access tokens after oauth:
-        webSocket.send("PASS oauth:7rgcke18dgqlo0tiinetfwq6m0ge1c");
+        webSocket.send("PASS oauth:userAccessToken");
         webSocket.send("NICK username");
        // webSocket.send("deadbeef".decodeHex());
         //webSocket.close(1000, "Goodbye, World!");
