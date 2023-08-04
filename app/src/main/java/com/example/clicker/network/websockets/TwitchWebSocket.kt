f4b6c96401ff6c02c66003d5feea03ce4e7d8a1a
Tristan Elliott <>
2023-08-03 21:59:53 -0300

    adding utilities to make the chat UI better

--- app/src/main/java/com/example/clicker/network/websockets/TwitchWebSocket.kt
+++ app/src/main/java/com/example/clicker/network/websockets/TwitchWebSocket.kt
@@ -12,6 +12,25 @@ import okio.ByteString
 import okio.ByteString.Companion.decodeHex
 import java.util.concurrent.TimeUnit
 
+data class TwitchUserData(
+    val badgeInfo: String?,
+    val badges: String?,
+    val clientNonce: String?,
+    val color: String?,
+    val displayName: String?,
+    val emotes: String?,
+    val firstMsg: String?,
+    val flags: String?,
+    val id: String?,
+    val mod: String?,
+    val returningChatter: String?,
+    val roomId: String?,
+    val subscriber: Boolean,
+    val tmiSentTs: Long?,
+    val turbo: Boolean,
+    val userId: String?,
+    val userType: String?
+)
 
 class TwitchWebSocket(): WebSocketListener() {
 
@@ -88,9 +107,17 @@ class TwitchWebSocket(): WebSocketListener() {
 
      override fun onMessage(webSocket: WebSocket, text: String) {
          val pattern ="PRIVMSG".toRegex()
-         pattern.find(text)?.let{
-             val substring = text.substring(it.range.first,text.length)
-             findLastIndex(substring)
+         val pattern2 = "#$streamerChannelName".toRegex()
+
+//         pattern.find(text)?.let{
+//             val substring = text.substring(it.range.first,text.length)
+//             findLastIndex(substring)
+//         }
+         val indexValue =text.indexOf("#$streamerChannelName").let { index ->
+             if(index != -1){
+                 val message = text.substring((index + "#$streamerChannelName".length +2),text.length)
+                 Log.d("websocketStoof","onMessage-> ${message}")
+             }
          }
 
     }
@@ -101,6 +128,8 @@ class TwitchWebSocket(): WebSocketListener() {
             _state.tryEmit(substring.substring(it.range.first+ 1,substring.length))
             Log.d("websocketStoof","onMessage: ${substring.substring(it.range.first+ 1,substring.length)}")
         }
+
+
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
@@ -116,3 +145,44 @@ class TwitchWebSocket(): WebSocketListener() {
         Log.d("websocketStoof","onFailure: ${webSocket.toString()}")
     }
 }
+
+
+fun parseStringBaby(input: String): Map<String, String> {
+    val pattern = "([^;@]+)=([^;]+)".toRegex()
+    val matchResults = pattern.findAll(input)
+
+    val parsedData = mutableMapOf<String, String>()
+
+
+    for (matchResult in matchResults) {
+        val (key, value) = matchResult.destructured
+        parsedData[key] = value
+    }
+
+    return parsedData
+}
+
+fun mapToTwitchUserData(parsedData: Map<String, String>): TwitchUserData {
+    return TwitchUserData(
+        badgeInfo = parsedData["badge-info"],
+        badges = parsedData["badges"],
+        clientNonce = parsedData["client-nonce"],
+        color = parsedData["color"],
+        displayName = parsedData["display-name"],
+        emotes = parsedData["emotes"],
+        firstMsg = parsedData["first-msg"],
+        flags = parsedData["flags"],
+        id = parsedData["id"],
+        mod = parsedData["mod"],
+        returningChatter = parsedData["returning-chatter"],
+        subscriber = parsedData["subscriber"]?.toIntOrNull() == 1,
+        roomId = parsedData["room-id"],
+        tmiSentTs = parsedData["tmi-sent"]?.toLongOrNull(),
+        turbo = parsedData["turbo"]?.toIntOrNull() == 1,
+        userType = parsedData["user-type"],
+        userId = parsedData["user-id"],
+    )
+}
+
+
+
