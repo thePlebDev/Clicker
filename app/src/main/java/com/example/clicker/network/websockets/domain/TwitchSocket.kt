2448d129182377770900854a2ec39b8b3220ef4d
Tristan Elliott <>
2023-11-09 03:58:29 -0400

    testing streamViewModel

--- app/src/main/java/com/example/clicker/network/websockets/domain/TwitchSocket.kt
+++ app/src/main/java/com/example/clicker/network/websockets/domain/TwitchSocket.kt
@@ -1,4 +1,28 @@
 package com.example.clicker.network.websockets.domain
 
+import com.example.clicker.network.websockets.models.LoggedInUserData
+import com.example.clicker.network.websockets.models.RoomState
+import com.example.clicker.network.websockets.models.TwitchUserData
+import kotlinx.coroutines.flow.StateFlow
+
 interface TwitchSocket {
+
+    val messageToDeleteId: StateFlow<String?>
+
+    val loggedInUserUiState: StateFlow<LoggedInUserData?>
+
+    val state: StateFlow<TwitchUserData>
+
+    val roomState: StateFlow<RoomState?>
+
+    fun run(
+        channelName: String?,
+        username: String
+    ): Unit
+
+     fun sendMessage(
+        chatMessage: String
+    ): Boolean
+
+    fun close(): Unit
 }
