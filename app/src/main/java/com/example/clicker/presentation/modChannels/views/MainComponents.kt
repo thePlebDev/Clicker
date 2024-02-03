4159b770fd4d9caa61bfc8746fde6a0b42be4fb4
Tristan Elliott <>
2024-02-03 07:48:47 -0400

    adding the fragment and starting compose view

---
+++ app/src/main/java/com/example/clicker/presentation/modChannels/views/MainComponents.kt
@@ -0,0 +1,17 @@
+package com.example.clicker.presentation.modChannels.views
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ModChannelView(){
+    TestingTHing()
+}
+
+@Composable
+fun TestingTHing(){
+    Text("MOD CHANNEL",color = MaterialTheme.colorScheme.onPrimary, fontSize = 30.sp)
+}
