6abc2e1c4c625ebdcaca21d004ae7a1058c57918
Tristan Elliott <>
2024-02-08 10:43:35 -0400

    adding the login with twitch logic to mod page

--- app/src/main/java/com/example/clicker/presentation/modChannels/ModChannelsFragment.kt
+++ app/src/main/java/com/example/clicker/presentation/modChannels/ModChannelsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.clicker.presentation.modChannels
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +17,7 @@ import com.example.clicker.ui.theme.AppTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.clicker.BuildConfig
 import com.example.clicker.presentation.home.HomeViewModel
 
 
@@ -46,10 +49,26 @@ class ModChannelsFragment : Fragment() {
         binding.composeView.apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
+                val clientId = BuildConfig.CLIENT_ID
+                val redirectUrl = BuildConfig.REDIRECT_URL
+                val twitchIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(
+                        "https://id.twitch.tv/oauth2/authorize?client_id=$clientId&redirect_uri=$redirectUrl&response_type=token&scope=user:read:follows+channel:moderate+moderation:read+chat:read+chat:edit+channel:read:editors+moderator:manage:chat_settings+moderator:read:automod_settings+moderator:manage:chat_messages+moderator:manage:automod_settings+moderator:manage:banned_users+user:read:moderated_channels"
+                    )
+                )
+
                 AppTheme{
                     ModChannelView(
                         onNavigate = {  findNavController().popBackStack() },
-                        homeViewModel = homeViewModel
+                        homeViewModel = homeViewModel,
+                        loginWithTwitch = {
+                            startActivity(twitchIntent)
+//                            intent.launchUrl(
+//                                requireActivity(),
+//                                Uri.parse(authorizationUrl)
+//                            )
+                        }
                     )
                 }
             }
