d449b9d9f8ed9c81164f32ae8230c4b728b23df0
Tristan Elliott <>
2024-02-10 11:42:50 -0400

    adding the basic functionality for the command parsing

--- app/src/test/java/com/example/clicker/network/repository/TwitchRepoImplTest.kt
+++ app/src/test/java/com/example/clicker/network/repository/TwitchRepoImplTest.kt
@@ -5,6 +5,8 @@ import com.example.clicker.network.clients.TwitchClient
 import com.example.clicker.network.domain.TwitchRepo
 import com.example.clicker.network.repository.util.TwitchStreamClientBuilder
 import com.example.clicker.presentation.stream.util.Scanner
+import com.example.clicker.presentation.stream.util.Token
+import com.example.clicker.presentation.stream.util.TokenType
 import com.example.clicker.util.Response
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.runTest
@@ -56,14 +58,17 @@ class TwitchRepoImplTest {
 
     @Test
     fun TestingTheLexicalAnanysis(){
-        val scanner = Scanner("/monitor @马思唯 马思唯 7777")
+        val scanner = Scanner("/ban @ turkeymeant44 ")
         scanner.scanTokens()
         val tokenList =scanner.tokenList
-        println("$tokenList")
+//        tokenCommand(tokenList)
+
+
 
         Assert.assertEquals(tokenList.size,4)
 
     }
+
 
 
 
