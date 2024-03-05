d494c4712a2a26e9453927b1bbcf0673ca65ed48
Tristan Elliott <>
2024-03-05 11:23:46 -0400

    adding the basic UI for the live long press channels

---
+++ app/src/main/java/com/example/clicker/presentation/stream/views/horizontalLongPress/LongPress.kt
@@ -0,0 +1,244 @@
+package com.example.clicker.presentation.stream.views.horizontalLongPress
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import com.example.clicker.R
+import com.example.clicker.presentation.sharedViews.ScaffoldBottomBarScope
+import com.example.clicker.presentation.sharedViews.ScaffoldTopBarScope
+
+@Composable
+fun HorizontalLongPressView(){
+    val clicked = remember { mutableStateOf(true) }
+    val text = if (clicked.value) "Live channels" else "Mod channels"
+
+    LongPress
+        .MainView(
+            topBar = {
+                TopBarText(text)
+            },
+            bottomBar = {
+                this.DualButtonNavigationBottomBar(
+                    bottomRowHeight = 50.dp,
+                    firstButton ={
+                        this.IconOverText(
+                            iconColor =if(clicked.value) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onPrimary,
+                            text = "Home",
+                            imageVector = Icons.Default.Home,
+                            iconContentDescription = "View live followed channels",
+                            onClick = {
+                                clicked.value = true
+                            }
+                        )
+                    } ,
+                    secondButton = {
+                        this.PainterResourceIconOverText(
+                            iconColor =if(!clicked.value) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onPrimary,
+                            text = "Mod Channels",
+                            painter = painterResource(id = R.drawable.mod_view_24),
+                            iconContentDescription = "View live mod channels",
+                            onClick = {
+                                clicked.value = false
+                            }
+                        )
+                    }
+                )
+            },
+            content = {
+
+            }
+        )
+}
+
+object LongPress{
+
+
+    @Composable
+    fun MainView(
+        topBar:@Composable ScaffoldTopBarScope.() -> Unit,
+        bottomBar:@Composable ScaffoldBottomBarScope.() -> Unit,
+        content:@Composable () -> Unit,
+
+        ) {
+        val topBarScaffoldScope = remember(){ScaffoldTopBarScope(35.dp)}
+        val bottomBarScaffoldScope = remember(){ScaffoldBottomBarScope(25.dp)}
+
+        Scaffold(
+            containerColor = MaterialTheme.colorScheme.primary,
+            topBar = {
+                Row(modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.primary)){
+                    with(topBarScaffoldScope){
+                        topBar()
+                    }
+                }
+
+            },
+            bottomBar = {
+                with(bottomBarScaffoldScope){
+                    bottomBar()
+                }
+            },
+
+        ) { contentPadding ->
+            //this should be its own component
+            TestingLazyColumnItem(contentPadding = contentPadding)
+        }
+    }
+}
+
+@Composable
+fun TestingLazyColumnItem(
+    contentPadding: PaddingValues,
+    ){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = contentPadding
+    ) {
+        item {
+            RowItem()
+        }
+        items(10){
+            RowItem()
+        }
+
+    }
+}
+
+@Composable
+fun RowItem(){
+    Row(
+        modifier =Modifier.fillMaxWidth().padding(5.dp)
+    ){
+        ImageWithViewCount(
+            url ="",
+            height = 50,
+            width = 50,
+            viewCount = 40000
+        )
+        StreamTitleWithInfo(
+            streamerName = "cohhcarnage",
+            gameTitle = "FINAL FANTASY VII REBIRTH",
+            streamTitle = "Final Fantasy VII: REBIRTH! - Thank you for $185,000+ raised during our !Charity event! - !MobileApp / !Emberville"
+
+        )
+
+
+    }
+}
+
+@Composable
+fun ImageWithViewCount(
+    url: String,
+    height: Int,
+    width: Int,
+    viewCount:Int,
+
+){
+    Log.d("ImageHeightWidth","url -> $url")
+    Box(
+        modifier = Modifier.height(70.dp).width(100.dp).background(Color.Red)
+    ) {
+
+        SubcomposeAsyncImage(
+            model = url,
+            loading = {
+                Column(modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp)
+                    .background(MaterialTheme.colorScheme.primary),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    CircularProgressIndicator()
+                }
+            },
+            contentDescription = stringResource(R.string.sub_compose_async_image_description)
+        )
+        Text(
+            "${viewCount}",
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                fontWeight = FontWeight.ExtraBold
+            ),
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(5.dp)
+        )
+    }
+}
+
+@Composable
+fun StreamTitleWithInfo(
+    streamerName:String,
+    streamTitle:String,
+    gameTitle:String
+){
+    Column(modifier = Modifier.padding(start = 10.dp)) {
+        Text(
+            streamerName,
+            fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(
+            streamTitle,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+            modifier = Modifier.alpha(0.7f),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(
+            gameTitle,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.alpha(0.7f),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+
