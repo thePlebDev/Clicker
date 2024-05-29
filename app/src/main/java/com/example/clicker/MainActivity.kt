ed2b1d89988e4bf8af9edf359769f6fa07d9bfd6
Tristan Elliott <>
2024-05-28 22:19:55 -0300

    removing the domain logic from the main activity

--- app/src/main/java/com/example/clicker/MainActivity.kt
+++ app/src/main/java/com/example/clicker/MainActivity.kt
@@ -29,77 +29,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val homeViewModel: HomeViewModel by viewModels()
 
-
-
-
-
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onResume() {
         super.onResume()
-        val context: Context = this
 
-
-        val minimunRequiredVersion = Build.VERSION_CODES.S
-        val deviceVersion = Build.VERSION.SDK_INT
-
-        if (deviceVersion >= minimunRequiredVersion) {
-            val manager = context.getSystemService(DomainVerificationManager::class.java)
-            val userState = manager.getDomainVerificationUserState(context.packageName)
-            val allowed = userState?.isLinkHandlingAllowed()
-            Log.d("domainManagerStuff", "allowed -> $allowed")
-
-            // Domains that haven't passed Android App Links verification but that the user
-            val another = DomainVerificationUserState.DOMAIN_STATE_SELECTED
-
-            val selectedDomain = userState?.hostToStateMap
-                ?.filterValues { it == DomainVerificationUserState.DOMAIN_STATE_SELECTED }
-
-            val verifiedDomains = userState?.hostToStateMap
-                ?.filterValues { it == DomainVerificationUserState.DOMAIN_STATE_VERIFIED }
-            Log.d("domainManagerStuff", "verifiedDomains -> $verifiedDomains")
-            // has associated with an app.
-            val selectedDomains = userState?.hostToStateMap
-                ?.filterValues { it == DomainVerificationUserState.DOMAIN_STATE_SELECTED }
-            Log.d("domainManagerStuff", "selectedDomains -> $selectedDomains")
-
-            // All other domains.
-            val unapprovedDomains = userState?.hostToStateMap
-                ?.filterValues { it == DomainVerificationUserState.DOMAIN_STATE_NONE }
-
-            Log.d("domainManagerStuff", "unapprovedDomains -> $unapprovedDomains")
-
-            if (selectedDomains!!.isNotEmpty()) {
-                homeViewModel.registerDomian(true)
-            }
-            if (unapprovedDomains!!.isNotEmpty()) {
-                homeViewModel.registerDomian(false)
-            }
-            if (!userState.isLinkHandlingAllowed) {
-                homeViewModel.registerDomian(false)
-            }
-        } else {
-            homeViewModel.registerDomian(true)
-        }
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
+
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         // Registers BroadcastReceiver to track network connection changes.
-
-
-
-
-
-
-
-
-
-
         System.setProperty("kotlinx.coroutines.debug", if (BuildConfig.DEBUG) "on" else "off")
         installSplashScreen()
 
