31cf78556ffb45e9a92dbf0b03f658530f4ed12f
Tristan Elliott <>
2024-09-04 21:34:05 -0300

    adding documentation to TwitchSocket

--- app/src/main/java/com/example/clicker/network/domain/TwitchSocket.kt
+++ app/src/main/java/com/example/clicker/network/domain/TwitchSocket.kt
@@ -8,53 +8,32 @@ import kotlinx.coroutines.flow.StateFlow
 /**
  * TwitchSocket is an interface that will act as the API for access all the information inside of the [TwitchWebSocket][com.example.clicker.network.websockets.TwitchWebSocket]
  *
- * - This interface consists of 5 abstract properties:
- * - [messageToDeleteId]
- * - [loggedInUserUiState]
- * - [state]
- * - [latestBannedUserId]
- * - [roomState]
+ * @property messageToDeleteId a [StateFlow] String representing the id of the message that was just deleted
+ * @property loggedInUserUiState a [StateFlow] [LoggedInUserData]  object used to represent the state of user's logged in status
+ * @property state a [StateFlow] [TwitchUserData] object that is used to represent the most recent user message sent from the Twitch IRC server
+ * @property latestBannedUserId a [StateFlow] string representing the id of the latest person to be banned or timed out.
+ * @property roomState a [StateFlow] [RoomState] object that is used to represent state of the current chat room
+ * @property hasWebSocketFailed a [StateFlow] Boolean object that is used to represent if the websocket has failed or not
+ * @property run a function used to start the websocket. It should first check if one is already running. If it is, shut it down and then create a new one
+ * @property sendMessage a function used to send a message to a chat session
+ * @property close a function used to close the current websocket
  *
- * - This interface consists of 3 abstract functions:
- * - [run]
- * - [sendMessage]
- * - [close]
  * */
 interface TwitchSocket {
 
-    /**
-     *  a [StateFlow] String representing the id of the message that was just deleted
-     * */
     val messageToDeleteId: StateFlow<String?>
-
-    /**
-     *  a [StateFlow] [LoggedInUserData] used to represent the state of user's logged in status
-     * */
     val loggedInUserUiState: StateFlow<LoggedInUserData?>
-
-    /**
-     *  a [StateFlow] [TwitchUserData] object that is used to represent the most recent user message sent from the Twitch IRC server
-     * */
     val state: StateFlow<TwitchUserData>
-
-    /**
-     *  A [StateFlow] string representing the id of the latest person to be banned or timed out.
-     * */
     val latestBannedUserId: StateFlow<String?>
-
-    /**
-     *  a [StateFlow] [RoomState] object that is used to represent state of the current chat room
-     * */
     val roomState: StateFlow<RoomState?>
-
-    /**
-     *  a [StateFlow] Boolean object that is used to represent if the websocket has failed or not
-     * */
     val hasWebSocketFailed: StateFlow<Boolean?>
 
     /**
-     *  a function used to start the websocket. It should first check if one is already running. If it is, shut it down
-     *  and then create a new one
+     * - run() is a function meant to be called whenever a new websocket needs to be created
+     *
+     * @param channelName a string representing the channel we want to establish a websocket connection to
+     * @param username a String representing the username of the logged in user
+     * @param oAuthToken a String representing a token that uniquely identifies the user's logged in session
      * */
     fun run(
         channelName: String?,
@@ -63,15 +42,16 @@ interface TwitchSocket {
     ): Unit
 
     /**
-     *  a function used to send the message to the Twitch IRC server. This is how the messages get send from our device and
-     *  are seen by other people on their devices
+     * - sendMessage() is a function meant to be called whenever a user wants to send a message to a user's chat session
+     *
+     * @param chatMessage a string representing the message a user is sending to the chat session
      * */
      fun sendMessage(
         chatMessage: String
     ): Boolean
 
     /**
-     *  a function used to close the websocket that is connected to the Twitch IRC server
+     *  - close() is a function meant to be called whenever a user wants to end the currently established websocket
      * */
     fun close(): Unit
 }
