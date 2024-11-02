9483a375dad72cfed6cd22cc114dcf2076a35093
Tristan Elliott <>
2024-11-02 14:33:42 -0300

    adding the basic chat with autoscrolling

--- app/src/main/java/com/example/clicker/presentation/stream/clearHorizontalChat/ClearHorizontalChat.kt
+++ app/src/main/java/com/example/clicker/presentation/stream/clearHorizontalChat/ClearHorizontalChat.kt
@@ -14,6 +14,11 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,7 +30,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.example.clicker.network.models.websockets.TwitchUserData
 import com.example.clicker.presentation.stream.StreamViewModel
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 
@@ -33,21 +41,25 @@ import kotlin.math.roundToInt
 fun ClearHorizontalChatView(
     streamViewModel:StreamViewModel
 ){
+    val twitchUserChat = streamViewModel.listChats.toList()
     DraggableClearChat(
-        (streamViewModel.fullImmersionWidth.value)*-1
+        (streamViewModel.fullImmersionWidth.value)*-1,
+        twitchUserChat=twitchUserChat,
     )
 
 }
 
 @Composable
 fun DraggableClearChat(
-    fullImmersionWidth:Int
+    fullImmersionWidth:Int,
+    twitchUserChat: List<TwitchUserData>,
 ){
     var offsetX by remember { mutableStateOf(0f) }
 
     val maxWidthHalf = (Resources.getSystem().displayMetrics.widthPixels/2.5)*-1
     var clearChatWidth by remember { mutableStateOf(0) }
     val scope = rememberCoroutineScope()
+
     val draggableState = DraggableState { delta ->
         offsetX += delta
     }
@@ -62,7 +74,6 @@ fun DraggableClearChat(
             .fillMaxHeight()
             .fillMaxWidth(.25f)
             .align(Alignment.CenterEnd)
-            .background(Color.Red.copy(alpha = 0.7f))
             .draggable(
                 orientation = Orientation.Horizontal,
                 state = draggableState,
@@ -97,9 +108,35 @@ fun DraggableClearChat(
             }
 
         ){
+            // this is where the chat needs to go
+            ClearChatLazyColumn(
+                twitchUserChat=twitchUserChat
+            )
 
         }
-        // Your content here
+
     }
 }
+@Composable
+fun ClearChatLazyColumn(
+    twitchUserChat: List<TwitchUserData>,
+){
+    var autoscroll by remember { mutableStateOf(true) }
+    val coroutineScope = rememberCoroutineScope()
+    val lazyColumnListState = rememberLazyListState()
+    LazyColumn(
+        modifier =Modifier.fillMaxSize().padding(10.dp),
+        state = lazyColumnListState
+    ){
+        coroutineScope.launch {
+            if (autoscroll) {
+                lazyColumnListState.scrollToItem(twitchUserChat.size)
+            }
+        }
+        items(twitchUserChat){twitchUser->
+            Text("${twitchUser.displayName} : ${twitchUser.userType}")
+        }
 
+    }
+
+}
