d6f80a90f05123ce7496ca6665650e0aa96d6e49
Tristan Elliott <>
2024-11-21 16:51:28 -0400

    adding the proper documentaiton to the data access object

--- app/src/main/java/com/example/clicker/data/room/DataAccessObjects.kt
+++ app/src/main/java/com/example/clicker/data/room/DataAccessObjects.kt
@@ -9,6 +9,16 @@ import com.example.clicker.network.clients.TopGame
 import kotlinx.coroutines.flow.Flow
 
 
+/**
+ * - **PinnedItemsDAO** is the interface that acts as the main Data Access Object for the underlying room database
+ * - you can read more about data access objects, [HERE](https://developer.android.com/training/data-storage/room/accessing-data)
+ *
+ * @property getAllPinnedItems a [Flow] containing a [MutableList] of [PinnedItem] objects. Meant to represent clicked
+ * category
+ *
+ * @property insertPinnedItem a function, when call with a [PinnedItem] object, is meant to store said object in the internal system
+ * @property deletePinnedItem a function, when call with a [PinnedItem] object, is meant to remove said object in the internal system
+ * */
 @Dao
 interface PinnedItemsDAO {
     @Query("SELECT * FROM pinned")
