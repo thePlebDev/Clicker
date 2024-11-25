8c35f22fb8b5f156192d4248005b56cd7984ba4e
Tristan Elliott <>
2024-11-24 23:48:34 -0400

    updating the documentation

--- app/src/main/java/com/example/clicker/network/clients/BetterTTVEmoteClient.kt
+++ app/src/main/java/com/example/clicker/network/clients/BetterTTVEmoteClient.kt
@@ -5,10 +5,17 @@ import com.example.clicker.network.models.emotes.IndivBetterTTVEmote
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+
+/**
+ * - **BetterTTVEmoteClient** is an interface that will act as the API to the BetterTTV emote servers
+ *
+ * @property getGlobalEmotes a function used to get the global BetterBTTV Emote
+ * @property getChannelEmotes a function used to get the channel specific BetterBTTV Emote
+ * */
 interface BetterTTVEmoteClient {
 
     /**
-     * - A GET request to the BetterTTV backend to request all global emotes.
+     * - **getGlobalEmotes** is a GET request to the BetterTTV backend to request all global emotes.
      * - You can read more about the BetterTTV documentation, [HERE](https://betterttv.com/developers/api#global-emotes)
      * */
     @GET("cached/emotes/global")
@@ -17,7 +24,7 @@ interface BetterTTVEmoteClient {
 
 
     /**
-     * - A GET request to the BetterTTV backend to request all channel specific emotes.
+     * - **getGlobalEmotes** is a GET request to the BetterTTV backend to request all channel specific emotes.
      * - You can read more about the BetterTTV documentation, [HERE](https://betterttv.com/developers/api#channel-emotes)
      *
      * @param broadcasterId a String representing the unique identifier of the Twitch streamer
