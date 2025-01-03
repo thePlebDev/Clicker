8254e7b3a5e5b83b37b38706a259a67be6483c76
Tristan Elliott <>
2025-01-02 23:42:24 -0400

    adding the camera preview

--- app/src/main/java/com/example/clicker/presentation/selfStreaming/SelfStreamingFragment.kt
+++ app/src/main/java/com/example/clicker/presentation/selfStreaming/SelfStreamingFragment.kt
@@ -1,10 +1,14 @@
 package com.example.clicker.presentation.selfStreaming
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,13 +16,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.example.clicker.R
 import com.example.clicker.databinding.FragmentHomeBinding
 import com.example.clicker.databinding.FragmentSelfStreamingBinding
 import com.example.clicker.presentation.selfStreaming.views.SelfStreamingView
+import com.google.common.util.concurrent.ListenableFuture
 
 
 class SelfStreamingFragment : Fragment() {
+
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
 
     private  var _binding: FragmentSelfStreamingBinding? = null
     /**
@@ -36,21 +45,42 @@ class SelfStreamingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        setUpCamera(requireActivity().applicationContext)
         // Inflate the layout for this fragment
         _binding = FragmentSelfStreamingBinding.inflate(inflater, container, false)
         val view = binding.root
-        binding.composeView.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                SelfStreamingView()
-            }
-        }
+//        binding.composeView.apply {
+//            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+//            setContent {
+//                SelfStreamingView()
+//            }
+//        }
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private fun setUpCamera(context:Context){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(context))
+    }
+    private fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+            .build()
+        val previewView =binding.previewView
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider())
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
     }
 
 
