8ffde05cd457153423c46aa484e16323cb2765b2
Tristan Elliott <>
2025-03-11 20:43:17 -0300

    adding new openGL rendere for dino run

---
+++ app/src/main/cpp/dino_run/dino_run.cpp
@@ -0,0 +1,60 @@
+//
+// Created by Tristan on 2025-03-11.
+//
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <android/log.h>
+#include <jni.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_init(JNIEnv *env, jobject thiz,
+                                                                        jint width, jint height) {
+
+}
---
+++ app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI.kt
@@ -0,0 +1,21 @@
+package com.example.clicker.presentation.minigames.dinoRun
+
+
+
+object DinoRunJNI{
+
+    init{
+        //todo: I need to make this file and add it to the CMakeList
+        System.loadLibrary("dino_run");
+    }
+
+
+    /**
+     * @param width the current view width
+     * @param height the current view height
+     */
+    external fun init(width: Int, height: Int)
+
+
+
+}
