56429deee332bfb82d15ff8c9c8de095b57cab05
Tristan Elliott <>
2025-03-13 23:22:13 -0300

    calling a compose method from the native code

--- app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/ComposeDinoRunViews.kt
+++ app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/ComposeDinoRunViews.kt
@@ -1,16 +1,22 @@
 package com.example.clicker.presentation.minigames.dinoRun
 
 import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-
-
+import com.example.clicker.presentation.minigames.PixelContainer
+import com.example.clicker.presentation.minigames.TextShadowTitle
+import com.example.clicker.presentation.minigames.views.PingPongView
 
 
 @Composable
@@ -19,16 +25,39 @@ fun ComposeDinoRunViews(
     modifier: Modifier
 ) {
     // Store a reference to PingPongView
+    val dinoRunViewRef = remember { mutableStateOf<DinoRunView?>(null) }
+    val value = remember {
+        mutableStateOf("START GAME")
+    }
+
+    LaunchedEffect(Unit) {
+        DinoRunJNI.setOnTextUpdateCallback { newText ->
+            value.value = newText
+        }
+    }
+
 
     Box(){
         AndroidView(
             factory = {
-                DinoRunView(context)
+                DinoRunView(context).apply {
+                    dinoRunViewRef.value = this // Store reference
+                }
+
             },
             modifier = modifier
         )
 
+        Box(
+            modifier = Modifier.align(Alignment.Center).clickable {
+              //  DinoRunJNI.triggerUpdate() // Call C++ to trigger the update
+            }
+        ) {
 
+
+                TextShadowTitle(value.value)
+
+        }
 
 
     }
--- app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI.kt
+++ app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI.kt
@@ -1,12 +1,27 @@
 package com.example.clicker.presentation.minigames.dinoRun
 
+import android.util.Log
 
 
 object DinoRunJNI{
 
+
     init{
         //todo: I need to make this file and add it to the CMakeList
         System.loadLibrary("dino_run");
+    }
+
+
+    private var onTextUpdate: ((String) -> Unit)? = null
+
+    @JvmStatic
+    fun setOnTextUpdateCallback(callback: (String) -> Unit) {
+        onTextUpdate = callback
+    }
+
+    @JvmStatic
+    fun updateTextFromNative(newText: String) {
+        onTextUpdate?.invoke(newText)
     }
 
 
@@ -17,6 +32,8 @@ object DinoRunJNI{
     external fun init(width: Int, height: Int)
     external fun step()
     external fun jump()
+//    external fun triggerUpdate()
+
 
 
 
