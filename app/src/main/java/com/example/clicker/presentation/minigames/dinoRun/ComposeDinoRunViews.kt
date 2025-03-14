e8b111093865a6df521312d70ffbd5b2f2afdae7
Tristan Elliott <>
2025-03-14 16:54:14 -0300

    adding the start game/ end game UI

--- app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/ComposeDinoRunViews.kt
+++ app/src/main/java/com/example/clicker/presentation/minigames/dinoRun/ComposeDinoRunViews.kt
@@ -1,35 +1,55 @@
 package com.example.clicker.presentation.minigames.dinoRun
 
 import android.content.Context
+import androidx.camera.video.VideoRecordEvent.Start
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.zIndex
+import com.example.clicker.presentation.minigames.ContainerColors
 import com.example.clicker.presentation.minigames.PixelContainer
+import com.example.clicker.presentation.minigames.PixelContainer2
+import com.example.clicker.presentation.minigames.TextShadow
 import com.example.clicker.presentation.minigames.TextShadowTitle
+import com.example.clicker.presentation.minigames.drawContainer
 import com.example.clicker.presentation.minigames.views.PingPongView
 import kotlinx.coroutines.delay
 
@@ -98,13 +118,18 @@ fun ComposeDinoRunViews(
         )
 
 
-        Box(modifier= Modifier.align(Alignment.TopCenter).padding(top=30.dp).clickable {
-            imageVisible.value = !imageVisible.value
-        }) {
+        Box(modifier= Modifier
+            .align(Alignment.TopCenter)
+            .padding(top = 30.dp)
+            .clickable {
+                imageVisible.value = !imageVisible.value
+            }) {
             TextShadowTitle("Score: ${timerClock.value}")
         }
         AnimatedVisibility(
-            modifier= Modifier.align(Alignment.CenterStart).padding(start = 20.dp,bottom=130.dp),
+            modifier= Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 20.dp, bottom = 130.dp),
             visible = imageVisible.value,
             enter = fadeIn(animationSpec = tween(1000)),
             exit = fadeOut(animationSpec = tween(1000))
@@ -113,9 +138,13 @@ fun ComposeDinoRunViews(
                 TextShadowTitle2("SPEED ↑")
             }
         }
+//        StartGameText(
+//            modifier = Modifier.align(Alignment.Center).padding(bottom = 70.dp)
+//        )
 
-
-
+//        GameOverText(
+//            modifier = Modifier.align(Alignment.Center).padding(bottom = 70.dp)
+//        )
 
 
 
@@ -123,10 +152,47 @@ fun ComposeDinoRunViews(
 
 
 }
+@Composable
+fun StartGameText(
+    modifier: Modifier
+){
+    Box(
+        modifier= modifier
+    ){
+
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            TextShadowTitle3("START GAME",35.sp,Color.Yellow)
+
+        }
+
+
+
+    }
+}
+
+
+@Composable
+fun GameOverText(
+    modifier: Modifier
+){
+    Box(
+        modifier= modifier
+    ){
+
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            TextShadowTitle3("GAME OVER!",35.sp,Color.Red)
+
+        }
+
+
+
+    }
+}
 
 @Composable
 fun TextShadowTitle2(
-    text:String
+    text:String,
+
 ) {
     val offset = Offset(5.0f, 10.0f)
 
@@ -150,5 +216,75 @@ fun TextShadowTitle2(
             textAlign = TextAlign.Center
         )
 
+    }
+}
+@Composable
+fun TextShadowTitle3(
+    text:String,
+    fontSize: TextUnit,
+    mainColor: Color
+) {
+    val offset = Offset(5.0f, 10.0f)
+
+    // Applying retro blocky text style with multiple shadows and custom styling
+    Column {
+        Text(
+
+
+            text = text,
+            style = TextStyle(
+                fontSize = fontSize,  // Larger font size for retro impact
+                fontWeight = FontWeight.Bold,  // Bold font for blocky look
+                color = mainColor,  // Bright color for a retro feel
+                letterSpacing = 3.sp,  // Increased letter spacing for a blocky, spaced out look
+                shadow = Shadow(
+                    color = Color.Blue,  // Red shadow for a retro 3D effect
+                    offset = Offset(4f, 4f),  // Shadow offset for depth
+                    blurRadius = 6f  // Blur radius to create a more retro 3D effect
+                ),
+            ),
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
+
+@Composable
+fun PixelContainer3(
+    enabled: Boolean = true,
+    clickable: Boolean = false,
+    onClick: (() -> Unit)? = null,
+    cornerSize: Int,
+    pixelSize: Dp = 4.dp,
+    colors: ContainerColors = ContainerColors.NintendoContainerColors,
+    content: @Composable () -> Unit,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    Box(
+        modifier = Modifier
+            .size(100.dp, 100.dp)
+            .aspectRatio(1f) // Makes the container a square
+            .clickable(
+                enabled = clickable,
+                onClick = { onClick?.invoke() },
+                indication = null,
+                interactionSource = interactionSource
+            )
+            .drawBehind {
+                drawContainer(
+                    colors = if (enabled) colors else ContainerColors.GreyContainerColors,
+                    pressedState = isPressed,
+                    cornerSize = cornerSize,
+                    pixelSize = pixelSize
+                )
+            }
+            .padding(
+                horizontal = (cornerSize.coerceAtLeast(1) + 1).toFloat() * pixelSize,
+                vertical = (cornerSize.coerceAtLeast(1) + 1).toFloat() * pixelSize // Ensuring vertical padding matches horizontal
+            )
+    ) {
+        content()
     }
 }
