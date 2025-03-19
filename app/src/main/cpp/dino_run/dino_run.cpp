f625628259590629b4f44cfe3f494cbd5960cba8
Tristan Elliott <>
2025-03-19 14:13:10 -0300

    adding new folder and actions

--- app/src/main/cpp/dino_run/dino_run.cpp
+++ app/src/main/cpp/dino_run/dino_run.cpp
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include "dino_run.h"
 #include "dino_run_setup.h"
+#include "dino_run_actions.h"
 
 #define LOG_TAG "DINORUN"
 #define LOGI(TAG, ...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
@@ -20,6 +21,7 @@
 
 //todo: READ UP ON POINTERS
 TransformShader* shaders = new TransformShader();
+Actions* actions = new Actions();
 
 
 
@@ -56,83 +58,6 @@ void showGameOverUI(JNIEnv *env){
     }
 }
 
-
-void resetSquare(std::vector<GLfloat>& vertices){
-
-    //this is a reset on the y-values
-    vertices[1] = -0.0375f;
-    vertices[3] =-0.0375f;
-    vertices[5] =0.0375f;
-    vertices[7] = -0.0375f;
-    vertices[9] = 0.0375f;
-    vertices[11] =0.0375f;
-
-}
-
-bool startJump = false;
-bool hitTop = false;
-//todo: velocity is causing some deformation issues
-// by I have created a temporary fix with resetSquare()
-float velocity = 0.0f;
-
-void jump(std::vector<GLfloat>& vertices) {
-    float highestPosition = 0.4f;
-    float lowestPosition = -0.0375f;
-
-    // highest and lowest points on the square
-    float verticesHighPoint = vertices[5]; // top-right Y position
-    float verticesLowPoint = vertices[1];  // bottom-left Y position
-
-
-
-
-    if (startJump) {
-        if (!hitTop) {
-
-            if (verticesHighPoint < highestPosition) {
-                float distanceToTop = highestPosition - verticesHighPoint;
-
-                velocity += -0.0025f * distanceToTop; // Loss of momentum as it gets closer
-                // Apply the movement to all vertices
-                for (int i = 1; i <= 12; i += 2) {
-                    if (vertices[i] < highestPosition) {
-                        vertices[i] += 0.025f + velocity;  // Apply velocity
-                    } else {
-                        vertices[i] = highestPosition; // Stop at the highest position
-                    }
-                }
-
-            } else {
-                velocity=0.0f;
-                hitTop = true;
-            }
-        } else {
-            // Simulate downward acceleration
-
-
-            if (verticesLowPoint > lowestPosition) {
-                for (int i = 1; i <= 12; i += 2) {
-
-                    if (vertices[i] > lowestPosition) {
-                        // velocity += -0.0001f;
-                        vertices[i] += (-0.035f); // Move down
-                    } else {
-                        vertices[i] = lowestPosition; // Stop at lowest position
-                    }
-                }
-            } else {
-
-
-                hitTop = false;
-                startJump = false;
-                velocity =0.0f;
-                resetSquare(vertices); // this is preventing the positioning issue that needs to be tackled
-            }
-        }
-
-    }
-}
-
 void resetSecondSquare(std::vector<GLfloat>& vertices){
     vertices[12] = 0.85f;
     vertices[14] =1.0f;
@@ -141,52 +66,11 @@ void resetSecondSquare(std::vector<GLfloat>& vertices){
     vertices[20] = 1.0f;
     vertices[22] =0.85f;
 }
-static JavaVM *javaVM = nullptr;
-
-
-// Store JVM reference when the game initializes
-extern "C"
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
-    javaVM = vm;
-    return JNI_VERSION_1_6;
-}
-
-//todo: we should be able to just pass in a JNIEnv *env instance
-//by just passing it to moveSecondSquare()
-void updateTextFromNative(const char *message) {
-    if (!javaVM) return;
-
-
-    //----------IGNORE BELOW------------
-    JNIEnv *env;
-    bool didAttach = false;
-
-    // Attach current thread if needed
-    if (javaVM->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-        javaVM->AttachCurrentThread(&env, nullptr);
-        didAttach = true;
-    }
-    //----------IGNORE ABOVE------------
-
-
-    //todo: need to figure out more about this
-        jclass dinoRunJNIClass = env->FindClass("com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI");
-        jmethodID updateTextMethod = env->GetStaticMethodID(dinoRunJNIClass, "updateTextFromNative", "(Ljava/lang/String;)V");
-
-        if (updateTextMethod) {
-
-            jstring jMessage = env->NewStringUTF(message);
-            env->CallStaticVoidMethod(dinoRunJNIClass, updateTextMethod, jMessage);
-            env->DeleteLocalRef(jMessage);
-        }
 
 
 
-    if (didAttach) {
-        javaVM->DetachCurrentThread();
-    }
-}
 
+//todo: this goes to the move section
 void showSpeedIncrease(JNIEnv *env){
     LOGI("showSpeedIncreaseTest",  "INCREASE THE SPEED");
     jclass dinoRunJNIClass = env->FindClass("com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI");
@@ -201,10 +85,28 @@ void showSpeedIncrease(JNIEnv *env){
 
 
 }
+
+
+//todo: we should be able to just pass in a JNIEnv *env instance
+//by just passing it to moveSecondSquare()
+void updateTextFromNative(const char *message,JNIEnv *env) {
+
+
+
+    //todo: need to figure out more about this
+    jclass dinoRunJNIClass = env->FindClass("com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI");
+    jmethodID updateTextMethod = env->GetStaticMethodID(dinoRunJNIClass, "updateTextFromNative", "(Ljava/lang/String;)V");
+
+    if (updateTextMethod) {
+
+        jstring jMessage = env->NewStringUTF(message);
+        env->CallStaticVoidMethod(dinoRunJNIClass, updateTextMethod, jMessage);
+        env->DeleteLocalRef(jMessage);
+    }
+
+}
 float secondSquareMovementSpeed = -0.02f;
 int successfulJumps = 0;
-
-
 void moveSecondSquare(std::vector<GLfloat>& vertices,JNIEnv *env){
     //these are the x-axis boudaries for the second square
     float rightBoundarySquareOne = vertices[14];
@@ -221,13 +123,13 @@ void moveSecondSquare(std::vector<GLfloat>& vertices,JNIEnv *env){
     //if(secondFarthestRight<=farthestLeft)
     // Corrected hit detection logic (checks for overlap)
     if (!(rightBoundarySquareOne < leftBoundarySquareTwo || leftBoundarySquareOne > rightBoundarySquareTwo)) {
-       // LOGI("farthestLeftTesting", "HIT!!!! RESET");
+        // LOGI("farthestLeftTesting", "HIT!!!! RESET");
 
         if (!(topBoundarySquareOne < bottomBoundarySquareTwo || bottomBoundarySquareOne > topBoundarySquareTwo)){
             LOGI("farthestLeftTesting", "Y-RANGE HIT");
             successfulJumps=0;
             secondSquareMovementSpeed = -0.02f;
-            updateTextFromNative("HIT");
+            updateTextFromNative("HIT",env);
             //todo: this needs to set gamestatus to over and show the game overUI
             showGameOverUI(env);
             resetSecondSquare(vertices);
@@ -241,11 +143,11 @@ void moveSecondSquare(std::vector<GLfloat>& vertices,JNIEnv *env){
         LOGI("farthestLeftTesting", "off screen");
         resetSecondSquare(vertices);
         successfulJumps+=1;
-  LOGI("speedUpdateTesting", "increase speed--->%d",(successfulJumps%5 ==0));
-  if((successfulJumps%5 ==0)){
-      showSpeedIncrease(env);
-      secondSquareMovementSpeed += -0.0025f;
-  }
+        LOGI("speedUpdateTesting", "increase speed--->%d",(successfulJumps%5 ==0));
+        if((successfulJumps%5 ==0)){
+            showSpeedIncrease(env);
+            secondSquareMovementSpeed += -0.0025f;
+        }
 
     }else{
         for(int i =12; i <23; i +=2){
@@ -257,6 +159,10 @@ void moveSecondSquare(std::vector<GLfloat>& vertices,JNIEnv *env){
 
 
 }
+/********ABOVE IS THE MOVEMENT SECTION***********/
+
+
+
 
 //this is not working as I expected
 void updateVerticesForAspectRatio(GLfloat* vertices, float aspectRatio) {
@@ -333,7 +239,7 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_step(JNIEnv *env, jobject thiz) {
     std::vector<GLfloat>& vertices = shaders->getSquareVertices();
-    jump(shaders->getSquareVertices());
+    actions->jump(shaders->getSquareVertices());
     LOGI("JUMPINGCHECK", "STEP!!!!!!!!!");
 
     switch(gameValue){
@@ -347,7 +253,7 @@ Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_step(JNIEnv *
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_jump(JNIEnv *env,jobject thiz) {
-    startJump = true;
+    actions->setStartJumpTrue();
     LOGI("JUMPINGCHECK", "JUMP");
     switch(gameValue){
         case init  : removeStartUI(env);   break;
@@ -358,24 +264,5 @@ Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_jump(JNIEnv *
 
 
 }
-//extern "C"
-//JNIEXPORT void JNICALL
-//Java_com_example_clicker_presentation_minigames_dinoRun_DinoRunJNI_triggerUpdate(JNIEnv *env,
-//                                                                                 jobject thiz) {
-//    jclass dinoRunJNIClass = env->FindClass("com/example/clicker/presentation/minigames/dinoRun/DinoRunJNI");
-//
-//    if (dinoRunJNIClass == nullptr) {
-//        return;
-//    }
-//
-//    jmethodID updateTextMethod = env->GetStaticMethodID(dinoRunJNIClass, "updateTextFromNative", "(Ljava/lang/String;)V");
-//
-//    if (updateTextMethod == nullptr) {
-//        return;
-//    }
-//
-//    jstring newText = env->NewStringUTF("MEATBALL");
-//    env->CallStaticVoidMethod(dinoRunJNIClass, updateTextMethod, newText);
-//    env->DeleteLocalRef(newText);
-//}
+
 
