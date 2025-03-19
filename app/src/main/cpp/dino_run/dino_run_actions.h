6b51976a3751f4f8bd2c87fc5a1a1d41d1edc690
Tristan Elliott <>
2025-03-19 14:51:27 -0300

    moving funcs to action class

--- app/src/main/cpp/dino_run/dino_run_actions.h
+++ app/src/main/cpp/dino_run/dino_run_actions.h
@@ -16,6 +16,8 @@
 
 #endif //CLICKER_DINO_RUN_ACTIONS_H
 
+enum GameStatus { init, start, stop };
+
 
 
 class Actions {
@@ -24,11 +26,25 @@ private:
     bool hitTop;
     float velocity;
     void resetSquare(std::vector<GLfloat>& vertices);
+    float secondSquareMovementSpeed = -0.02f;
+    int successfulJumps = 0;
+    GameStatus gameValue = init;
 
 public:
     Actions();
     void jump(std::vector<GLfloat>& vertices);
     void setStartJumpTrue();
+    void moveSecondSquare(std::vector<GLfloat>& vertices,JNIEnv *env);
+    void updateTextFromNative(const char *message,JNIEnv *env);
+    void showGameOverUI(JNIEnv *env);
+    void resetSecondSquare(std::vector<GLfloat>& vertices);
+    void showSpeedIncrease(JNIEnv *env);
+    void removeStartUI(JNIEnv *env);
+
+    //todo: this needs to be looked into
+    GameStatus getGameValue() {
+        return gameValue;
+    }
 
 
 
